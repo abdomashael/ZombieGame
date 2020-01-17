2eb0e159c351b30b04fc794814f1177d24e257a2
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-01-17 21:18:00 +0200

    HowToPlay

--- js/HowToPlayScene.js
+++ js/HowToPlayScene.js
@@ -6,9 +6,28 @@ var HowToPlayScene = new Phaser.Class({
             Phaser.Scene.call(this, { key: 'HowToPlayScene' });
         },
     preload: function () {
-        this.load.image('back', 'assets/back.png');
-        this.load.image('ground', 'assets/platform.png');
+        this.load.image('back', 'assets/UP.png');
+        this.load.image('up', 'assets/UP.png');
+        this.load.image('space', 'assets/Space.png');
+        this.load.image('sewer1', 'assets/sewer1.png');
+        this.load.image('Attacker', 'assets/Attackericon.png');
+        this.load.image('Attacker', 'assets/Attackericon.png');
     },
-    create: function () { },
-    update: function () { }
+    create: function () {
+        txt = this.make.text({
+            x: 1500,
+            y: 100,
+            text: 'Welcome To Our Game',
+            origin: { x: 0.5, y: 0.5 },
+            style: {
+                font: 'bold 80px Algerian',
+                fill: '#ffdf94',
+                wrap: {
+                    mode: 'word',
+                    width: 300
+                }
+            }
+        });
+     },
+    update: function () {}
 });
