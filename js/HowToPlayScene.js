dd6098a4aa8bad43e4b54ebb00f98831b749487b
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-01-17 21:59:09 +0200

    fixes

--- js/HowToPlayScene.js
+++ js/HowToPlayScene.js
@@ -6,7 +6,7 @@ var HowToPlayScene = new Phaser.Class({
             Phaser.Scene.call(this, { key: 'HowToPlayScene' });
         },
     preload: function () {
-        this.load.image('back', 'assets/UP.png');
+        this.load.image('back', 'assets/backhowtoplay.jpg');
         this.load.image('up', 'assets/UP.png');
         this.load.image('space', 'assets/Space.png');
         this.load.image('sewer1', 'assets/sewer1.png');
@@ -14,8 +14,10 @@ var HowToPlayScene = new Phaser.Class({
         this.load.image('Attacker', 'assets/Attackericon.png');
     },
     create: function () {
-        txt = this.make.text({
-            x: 1500,
+        background = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'back');
+        console.log(txt);
+        var txt = this.make.text({
+            x: 1000,
             y: 100,
             text: 'Welcome To Our Game',
             origin: { x: 0.5, y: 0.5 },
