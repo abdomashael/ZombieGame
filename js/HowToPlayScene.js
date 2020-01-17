53215a2ad5b86c3ce1ffde6cd3d4806b6616593f
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-01-18 01:19:36 +0200

    Update HowToPlayScene.js

--- js/HowToPlayScene.js
+++ js/HowToPlayScene.js
@@ -21,8 +21,8 @@ var HowToPlayScene = new Phaser.Class({
         //UP = this.add.image(150, 300, 'up');
         //Space = this.add.image(100, 300, 'space');
         sewer = this.add.image(1800, 625, 'sewer1');
-        var Anim = this.physics.add.sprite(345, 550, 'Anim');
-        var Attacker = this.physics.add.sprite(700, 300, 'attacker');
+        var Anim = this.physics.add.sprite(345, 835, 'Anim');
+        var Attacker = this.physics.add.sprite(1850, 445, 'attacker');
 
         //incearsing the scaie of the anime
         Anim.setScale(0.5);
@@ -43,8 +43,8 @@ var HowToPlayScene = new Phaser.Class({
 
         //adding a ground to stop the animation
         platforms = this.physics.add.staticGroup();
-        platforms.create(600, 980, 'ground');
-        this.physics.add.collider(A, platforms);
+        platforms.create(700, 980, 'ground');
+        this.physics.add.collider(Anim, platforms);
         platforms.create(900, 550, 'ground');
         this.physics.add.collider(Attacker, platforms);
 
@@ -66,11 +66,12 @@ var HowToPlayScene = new Phaser.Class({
             }
         });
         //add the paragraphs
-        Texth = this.add.text(0, 300, 'Hello to the guide of our game we are pleased you are here and now let us start', { fontSize: '30px', fill: '#ffff' });
-        Textup = this.add.text(50, 400, 'First you just need to click on space or the up to jump ', { fontSize: '30px', fill: '#ffff' });
-        Textattacker = this.add.text(100, 500, 'Please watch out from this zombie eater plant or you will lose on of your loyal friends', { fontSize: '30px', fill: '#ffff' });
-        Textatsewer = this.add.text(150, 600, 'the last thing we want to tell you is that if you fell in the sewer you will lose', { fontSize: '30px', fill: '#ffff',fontFamily:'Calibri'});
-        Textatfinally = this.add.text(200, 700, 'We hope you enjoy our game and We wish you the best of luck beacuse you will need it', { fontSize: '30px', fill: '#ffff' });
+        Texth = this.add.text(0, 200, 'Hello to the guide of our game we are pleased you are here and now let us start', { fontSize: '35px', fill: '#ffff' ,fontFamily: 'Courier',fontStyle: 'bold italic' });
+        Texth = this.add.text(0, 300, 'All you have to do is that Just eat some victims to gain more zombie friends', { fontSize: '35px', fill: '#ffff' ,fontFamily: 'Courier',fontStyle: 'bold italic' });
+        Textup = this.add.text(0, 400, 'First you just need to click on space or the up buttons to jump to avoid attackers', { fontSize: '35px', fill: '#ffff',fontFamily: 'Courier',fontStyle: 'bold italic'  });
+        Textattacker = this.add.text(0, 500, 'Please watch out from this zombie eater plant or you will lose on of your loyal friends', { fontSize: '35px', fill: '#ffff',fontFamily: 'Courier',fontStyle: 'bold italic' });
+        Textatsewer = this.add.text(0, 600, 'The last thing we want to tell you is that if you fell in the sewer you will lose', { fontSize: '35px', fill: '#ffff',fontFamily: 'Courier',fontStyle: 'bold italic'});
+        Textatfinally = this.add.text(0, 700, 'We hope you enjoy our game and We wish you the best of luck beacuse you will need it', { fontSize: '35px', fill: '#ffff', fontFamily: 'Courier',fontStyle: 'bold italic'});
 
     },
     update: function () { }
