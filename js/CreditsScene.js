581ea2aace7dfc4205dc938a57811074cb6236e9
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-01-18 03:57:43 +0200

    Fixes

--- js/CreditsScene.js
+++ js/CreditsScene.js
@@ -8,11 +8,20 @@ var CreditsScene = new Phaser.Class({
             Phaser.Scene.call(this, { key: 'creditsScene' });
         },
         preload: function(){
+            //load the menuebutton to the scene
+            this.load.image('back', 'assets/backHowToPlay.png');
             this.load.spritesheet('menubtn', 'assets/menuBtn.png', { frameWidth: 400, frameHeight: 92 });
         },
         create: function(){
+            //add images
+            background = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'back');
             var menubtn = this.add.sprite(200, 900, 'menubtn').setInteractive();
+
+            //create animation
             createAnimation(this, "menubtnchange", "menubtn", 1, 0, 4);
+
+
+            //addtitle
             txt = this.make.text({
                 x: 1000,
                 y: 300,
@@ -27,11 +36,14 @@ var CreditsScene = new Phaser.Class({
                     }
                 }
             });
-            Text1 = this.add.text(1000, 200, 'This Game Was Brought To By:', { fontSize: '45px', fill: '#ffff', fontFamily: 'Courier', fontStyle: 'bold italic' });
-            Text2 = this.add.text(1000, 200, 'Abd ElRahman Mashaal', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
-            Text2 = this.add.text(1000, 200, 'Omar Akram', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
-            Text2 = this.add.text(1000, 200, 'Menna Abdallah', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
-            Text2 = this.add.text(1000, 200, 'Samar Ali Abd ElFatah', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
+
+
+            //add names
+            Text1 = this.add.text(1000, 0, 'This Game Was Brought To By:', { fontSize: '45px', fill: '#ffff', fontFamily: 'Courier', fontStyle: 'bold italic' });
+            Text2 = this.add.text(1000, 100, 'Abd ElRahman Mashaal', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
+            Text3 = this.add.text(1000, 200, 'Omar Akram', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
+            Text4 = this.add.text(1000, 300, 'Menna Abdallah', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
+            Text5 = this.add.text(1000, 400, 'Samar Ali Abd ElFatah', { fontSize: '45px', fill: '#ffd800', fontFamily: 'Courier', fontStyle: 'bold italic' });
 
              
 
@@ -53,19 +65,18 @@ var CreditsScene = new Phaser.Class({
             });
         },
         update: function(){
+            //add animation to names
             Text1.y += vy;
             Text2.y += vy;
             Text3.y += vy;
             Text4.y += vy;
             Text5.y += vy;
-            Text6.y += vy;
             if (txt.y > 2000) {
                 Text1.y = 0;
                 Text2.y = 0;
                 Text3.y = 0;
                 Text4.y = 0;
                 Text5.y = 0;
-                Text6.y = 0;
             }
         }
 
