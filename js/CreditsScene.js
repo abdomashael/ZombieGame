1aa45765f737482ecae69ab92ad227111334f234
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-01-18 03:10:16 +0200

    chnages

---
+++ js/CreditsScene.js
@@ -0,0 +1,12 @@
+var CreditsScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+    initialize:
+        function CreditsScene() {
+            Phaser.Scene.call(this, { key: 'creditsScene' });
+        },
+        preload: function(){},
+        create: function(){},
+        update: function(){},
+
+    })
